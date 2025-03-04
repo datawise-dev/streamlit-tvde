e60af44b877aaa8534dcd25796fd9b9592eef012
João Ceia <joao.p.ceia@gmail.com>
2025-03-04 21:20:46 +0000

    changed cars to display as page

--- views/cars/__init__.py
+++ views/cars/__init__.py
@@ -1,16 +1,15 @@
-from .form import show_car_form
-from .list import show_car_list
+from views.cars.form import show_car_form
+from views.cars.list import show_car_list
 
-def show_car_view():
-    """Main car view combining form and list functionalities."""
-    import streamlit as st
+"""Main car view combining form and list functionalities."""
+import streamlit as st
+
+st.title("Car Management")
+
+tab1, tab2 = st.tabs(["Add Car", "View/Edit Cars"])
+
+with tab1:
+    show_car_form()
     
-    st.title("Car Management")
-    
-    tab1, tab2 = st.tabs(["Add Car", "View/Edit Cars"])
-    
-    with tab1:
-        show_car_form()
-        
-    with tab2:
-        show_car_list()
+with tab2:
+    show_car_list()
