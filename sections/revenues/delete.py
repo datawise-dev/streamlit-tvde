93f58ff4bee8f573be90458bcef28fa5383bb0f5
João Ceia <joao.p.ceia@gmail.com>
2025-04-07 00:52:25 +0100

    added bulk_delete_revenues

--- sections/revenues/delete.py
+++ sections/revenues/delete.py
@@ -1,6 +1,7 @@
 import streamlit as st
+from typing import List
 from sections.revenues.service import RevenueService
-from utils.delete_helpers import generic_record_delete
+from utils.delete_helpers import generic_record_delete, generic_bulk_delete
 
 def delete_revenue(record_id):
     """Página para eliminar um registo de receita usando a função genérica."""
@@ -10,4 +11,13 @@ def delete_revenue(record_id):
         service=RevenueService,
         entity_name="receita",
         return_page="sections/revenues/page.py"
+    )
+
+def bulk_delete_revenues(record_ids: List[int]):
+    """Dialog to confirm deletion of multiple Revenue records."""
+    generic_bulk_delete(
+        record_ids=record_ids,
+        service_class=RevenueService,
+        entity_name="receita",
+        session_key="revenues_data_loaded"
     )
