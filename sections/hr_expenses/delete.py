4b532833ff0d15db0227702020eb0ae8f3c669db
João Ceia <joao.p.ceia@gmail.com>
2025-04-06 23:16:48 +0100

    fixed hr_expense_delete

--- sections/hr_expenses/delete.py
+++ sections/hr_expenses/delete.py
@@ -4,16 +4,11 @@ from utils.delete_helpers import generic_delete_page
 
 
 @st.dialog("Eliminar Despesa RH")
-def hr_expense_delete(expense_id, driver_name=None):
+def hr_expense_delete(expense_id):
     """Dialog to confirm deletion of an HR expense record."""
-    message = None
-    if driver_name:
-        message = f"Tem a certeza que deseja eliminar a despesa de **{driver_name}**?"
-        
     generic_delete_page(
         entity_id=expense_id,
         entity_name="despesa RH",
         service_class=HRExpenseService,
-        redirect_path="sections/hr_expenses/page.py",
-        custom_message=message
+        redirect_path="sections/hr_expenses/page.py"
     )
